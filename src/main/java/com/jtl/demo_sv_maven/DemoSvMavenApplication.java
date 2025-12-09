5c8c9c5c430439b3275a25efc3d72f76aa4c5743
santimc31 <cayetanosantiago63@gmail.com>
2025-12-09 01:41:30 -0500

    Incorporación incial del proyecto

---
+++ src/main/java/com/jtl/demo_sv_maven/DemoSvMavenApplication.java
@@ -0,0 +1,13 @@
+package com.jtl.demo_sv_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSvMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSvMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtl/demo_sv_maven/DemoSvMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtl.demo_sv_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSvMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
